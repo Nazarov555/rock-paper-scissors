4308dd3e16cdca5435afb58a6e9862e3f097ec41
Nazarov555 <nzrvkeskil@mail.ru>
2022-08-17 20:43:24 +0300

    skgndg

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rock-paper-scissors/',
+  base: './rock-paper-scissors/',
   plugins: [vue()]
 })
